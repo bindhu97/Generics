ea60eb4cd2609c1a57453d68f14d56f2bfb6fa00
bindhu97 <binduvishwakarma97@gmail.com>
2022-12-14 23:43:06 +0530

    [Bindhu]Maximum of Three Integers

---
+++ Generics/GenericMaximum/MaximumNumber.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generics.GenericMaximum
+{
+    public class MaximumNumber
+    {
+        public int MaxNumber(int firstNumber, int secondNumber, int thirdNumber)
+        {
+            if (firstNumber.CompareTo(secondNumber) > 0 && firstNumber.CompareTo(thirdNumber) > 0)
+            {
+                return firstNumber;
+            }
+            if (secondNumber.CompareTo(firstNumber) > 0 && secondNumber.CompareTo(thirdNumber) > 0)
+            {
+                return secondNumber;
+            }
+            if (thirdNumber.CompareTo(firstNumber) > 0 && thirdNumber.CompareTo(secondNumber) > 0)
+            {
+                return thirdNumber;
+            }
+        }
+    }
+}
