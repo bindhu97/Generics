2eef614d42836e6ee7b4e312dedba668e06c5416
bindhu97 <binduvishwakarma97@gmail.com>
2022-12-15 00:03:23 +0530

    [Bindhu]Maximum Number Test

---
+++ Generics/MaximumNumberTest/MaxNumberTest.cs
@@ -0,0 +1,19 @@
+﻿using Generics.GenericMaximum;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generics.MaximumNumberTest
+{
+    internal class MaxNumberTest
+    {
+        public void GivenIntegerValue_ReturnMaximum()
+        {
+            MaximumNumber maximum = new MaximumNumber();
+            int data = maximum.MaxNumber(15, 20, 30);
+            MaximumNumber.Equals(30, data);
+        }
+    }
+}
