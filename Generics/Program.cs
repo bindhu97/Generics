c832d57751b48f0ad7aa81525f2f8841b6c264f9
bindhu97 <binduvishwakarma97@gmail.com>
2022-12-15 00:13:22 +0530

    [Bindhu]Maximum of three float numbers

--- Generics/Program.cs
+++ Generics/Program.cs
@@ -1,6 +1,8 @@
 ﻿using Generics.GenericMaximum;
 
 MaximumNumber Maximum = new MaximumNumber();
-Maximum.MaxNumber(15, 20, 30);
+//Maximum.MaxNumber(15, 20, 30);
+Maximum.MaxNumber(15.5, 20.2, 30.3);
+
 
 
